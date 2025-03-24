3eb5fd2439d998c7cbf21e5501e4f749768c286b
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2025-03-23 20:23:21 -0700

    feat: add support for tf types that based on basetypes

--- attrtypes.go
+++ attrtypes.go
@@ -15,6 +15,7 @@ import (
 	"reflect"
 
 	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
 
 // AttributeTypes returns a map of attribute types for the specified type T.
@@ -29,7 +30,7 @@ func AttributeTypes[T any](ctx context.Context) (map[string]attr.Type, error) {
 	}
 
 	attributeTypes := make(map[string]attr.Type)
-	for i := 0; i < typ.NumField(); i++ {
+	for i := range typ.NumField() {
 		field := typ.Field(i)
 		if field.PkgPath != "" {
 			continue // Skip unexported fields.
@@ -56,8 +57,36 @@ func AttributeTypesMust[T any](ctx context.Context) map[string]attr.Type {
 
 // ElementType returns the element type of the specified type T.
 // T must be a slice or map and reflection is used to find the element type.
-func ElementType[T any](_ context.Context) (attr.Type, error) {
+func ElementType[T any](ctx context.Context) (attr.Type, error) {
 	var t T
+
+	supportedTypes := []attr.Type{
+		basetypes.StringType{},
+		basetypes.BoolType{},
+		basetypes.Int32Type{},
+		basetypes.Int64Type{},
+		basetypes.Float32Type{},
+		basetypes.Float64Type{},
+	}
+
+	if v, ok := any(t).(attr.Value); ok {
+		vType := v.Type(ctx)
+
+		vTypeReflect := reflect.TypeOf(vType)
+		for _, supportedType := range supportedTypes {
+			supportedTypeReflect := reflect.TypeOf(supportedType)
+
+			// Check if types match directly or if embedded type matches
+			if vTypeReflect == supportedTypeReflect ||
+				(vTypeReflect.Kind() == reflect.Struct && vTypeReflect.NumField() > 0 &&
+					vTypeReflect.Field(0).Type == supportedTypeReflect) {
+				return vType, nil
+			}
+		}
+
+		return nil, fmt.Errorf("%T has unsupported type: %s", t, vType.String())
+	}
+
 	val := reflect.ValueOf(t)
 	typ := val.Type()
 
